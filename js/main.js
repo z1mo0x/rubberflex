dd6bce5626e8ffe62eab69f5f5e4bf32f74b2ac9
z1mo0x <droweckiy@mail.ru>
2024-06-14 21:19:54 +0300

    first

--- js/main.js
+++ js/main.js
@@ -13,9 +13,29 @@ window.addEventListener("DOMContentLoaded", () => {
         sidebarContent.classList.toggle('menu-active');
     })
 
-    if (window.screenX < 993) {
-        headerMenuButton.addEventListener('click', () => {
-            sidebarContent.classList.toggle('menu-active');
-        })
+    // if (window.screenX < 993) {
+    //     headerMenuButton.addEventListener('click', () => {
+    //         sidebarContent.classList.toggle('menu-active');
+    //     })
+    // }
+
+    //sliders 
+
+    function heroSlider() {
+        const heroSlider = new Swiper('.hero__slider', {
+
+            pagination: {
+                el: ".hero__slider-pagination",
+                type: "fraction",
+            },
+
+            navigation: {
+                nextEl: ".hero__slider-next",
+                prevEl: ".hero__slider-prev",
+            },
+
+        });
     }
+
+    heroSlider()
 });
