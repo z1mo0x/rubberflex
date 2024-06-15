0002156b2d00a67b41f8f07b2d1c9951a48dacc2
z1mo0x <droweckiy@mail.ru>
2024-06-15 15:24:31 +0300

    first

--- js/main.js
+++ js/main.js
@@ -19,6 +19,62 @@ window.addEventListener("DOMContentLoaded", () => {
     //     })
     // }
 
+
+    //maskphone 
+    //maskphone 
+    var inputElements = document.querySelectorAll(".maskphone");
+
+    for (let inputElement of inputElements) {
+        console.log(inputElement);
+
+        inputElement.addEventListener("input", mask);
+        inputElement.addEventListener("focus", mask);
+        inputElement.addEventListener("blur", mask);
+
+        function mask(event) {
+            var blank = "+_ (___) ___-__-__";
+
+            var i = 0;
+            var val = this.value.replace(/\D/g, "").replace(/^8/, "7");
+            // <---
+
+            this.value = blank.replace(/./g, function (char) {
+                if (/[_\d]/.test(char) && i < val.length)
+                    return val.charAt(i++);
+
+                return i >= val.length ? "" : char;
+            });
+
+            if (event.type == "blur") {
+                if (this.value.length == 2)
+                    this.value = "";
+            } else {
+                setCursorPosition(this, this.value.length);
+            }
+        };
+
+        function setCursorPosition(elem, pos) {
+            elem.focus();
+
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+                return;
+            }
+
+            if (elem.createTextRange) {
+                var range = elem.createTextRange();
+                range.collapse(true);
+                range.moveEnd("character", pos);
+                range.moveStart("character", pos);
+                range.select();
+                return;
+            }
+        }
+
+    }
+
+
+
     //sliders 
 
     function heroSlider() {
@@ -38,5 +94,24 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    heroSlider()
+    function companySlider() {
+        const companySlider = new Swiper('.company__slider', {
+
+            pagination: {
+                el: ".company__pagination",
+            },
+
+
+            speed: 500,
+
+            navigation: {
+                nextEl: ".company-next",
+                prevEl: ".company-prev",
+            },
+
+        });
+    }
+
+    heroSlider();
+    companySlider();
 });
