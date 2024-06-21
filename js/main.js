013c6d9287c0084803709cf9162a1c2c93275d6f
z1mo0x <120253137+z1mo0x@users.noreply.github.com>
2024-06-21 13:38:22 +0300

    sad

--- js/main.js
+++ js/main.js
@@ -21,9 +21,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const div = document.querySelector('.menu');
 
-    $(document).mouseup( function(e){ // событие клика по веб-документу
-            if ( !div.is(e.target) // если клик был не по нашему блоку
-                && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
+    $(document).mouseup( function(e){ 
+            if ( !div.is(e.target) && div.has(e.target).length === 0 ) {
                 sidebarContent.classList.remove('menu-active')
         }
     });
