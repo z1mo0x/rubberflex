c3532fcd1dd1a3559d7dc55b15b75c1ebd0142c9
z1mo0x <120253137+z1mo0x@users.noreply.github.com>
2024-06-21 13:36:45 +0300

    ASD

--- js/main.js
+++ js/main.js
@@ -19,6 +19,15 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     })
 
+    const div = document.querySelector('.menu');
+
+    $(document).mouseup( function(e){ // событие клика по веб-документу
+            if ( !div.is(e.target) // если клик был не по нашему блоку
+                && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
+                sidebarContent.classList.remove('menu-active')
+        }
+    });
+
     downMenuButtons.forEach((element, index) => {
         element.addEventListener('click', (e) => {
             downMenuSecond.forEach((el) => {
